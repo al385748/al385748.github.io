59e7076e0d78666722fc7d8ad89274fc4f6d8fc8
al385748 <72459880+al385748@users.noreply.github.com>
2021-10-02 22:14:40 +0200

    GO FULLSCREEN

--- js/main.js
+++ js/main.js
@@ -22,6 +22,30 @@
 
 		screen.orientation.lock('landscape');
 
+		function waxon_tm_cursor(){
+
+			var myCursor	= jQuery('.mouse-cursor');
+		
+			if(myCursor.length){
+				if ($("body")) {
+				const e = document.querySelector(".cursor-inner"),
+					t = document.querySelector(".cursor-outer");
+				let n, i = 0,
+					o = !1;
+				window.onmousemove = function (s) {
+					o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+				}, $("body").on("mouseenter", "a, .cursor-pointer", function () {
+					e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
+				}), $("body").on("mouseleave", "a, .cursor-pointer", function () {
+					$(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
+				}), e.style.visibility = "visible", t.style.visibility = "invisible"
+			}
+			}
+		};
+
+		document.body.style.cursor = 'none';
+		waxon_tm_cursor();
+		
       });       
 
   	})
@@ -282,12 +306,17 @@
 
 			if (jQuery(window).scrollTop() >= pxShow) {
 				jQuery("#go-top").fadeIn(fadeInTime);
+
 			} else {
 				jQuery("#go-top").fadeOut(fadeOutTime);
 			}
 
 		}		
 
-	});		
+	});	
 
 })(jQuery);
+
+function goFullscreen(){
+	document.getElementById("top").requestFullscreen()
+}
